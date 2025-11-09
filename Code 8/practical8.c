d1e7d7b8431840e01d4ad31f7b73958125e076dc
24070993 <24070993@ycce.in>
2025-11-09 19:55:31 +0530

    Update and rename practical8.c to Code 8/practical8.c

--- Code 8/practical8.c
+++ Code 8/practical8.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure to represent an edge
 struct Edge {
     int src, dest, weight;
 };
-
-// Structure to represent a subset for union–find
 struct Subset {
     int parent;
     int rank;
 };
 
-// Function to find set of an element i (uses path compression)
 int find(struct Subset subsets[], int i) {
     if (subsets[i].parent != i)
         subsets[i].parent = find(subsets, subsets[i].parent);
     return subsets[i].parent;
 }
 
-// Function to perform union of two sets (uses union by rank)
 void Union(struct Subset subsets[], int x, int y) {
     int xroot = find(subsets, x);
     int yroot = find(subsets, y);
@@ -34,47 +29,37 @@ void Union(struct Subset subsets[], int x, int y) {
     }
 }
 
-// Compare function for qsort()
 int compareEdges(const void *a, const void *b) {
     struct Edge *a1 = (struct Edge *)a;
     struct Edge *b1 = (struct Edge *)b;
     return a1->weight - b1->weight;
 }
 
-// Function to construct MST using Kruskal’s algorithm
 void KruskalMST(struct Edge edges[], int V, int E) {
-    struct Edge result[V]; // This will store the resultant MST
-    int e = 0; // Index for result[]
-    int i = 0; // Index for sorted edges
+    struct Edge result[V]; 
+    int e = 0; 
+    int i = 0;
 
-    // Step 1: Sort all the edges in non-decreasing order of their weight
     qsort(edges, E, sizeof(struct Edge), compareEdges);
 
-    // Allocate memory for creating V subsets
     struct Subset *subsets = (struct Subset *)malloc(V * sizeof(struct Subset));
-
-    // Create V subsets with single elements
     for (int v = 0; v < V; v++) {
         subsets[v].parent = v;
         subsets[v].rank = 0;
     }
 
-    // Number of edges to be taken is equal to V-1
     while (e < V - 1 && i < E) {
-        // Step 2: Pick the smallest edge and increment index for next iteration
         struct Edge next_edge = edges[i++];
 
         int x = find(subsets, next_edge.src);
         int y = find(subsets, next_edge.dest);
 
-        // If including this edge doesn’t cause a cycle, include it
         if (x != y) {
             result[e++] = next_edge;
             Union(subsets, x, y);
         }
     }
 
-    // Print the contents of the MST
     printf("\nEdges in the Minimum Spanning Tree (MST):\n");
     int totalWeight = 0;
     for (i = 0; i < e; i++) {
@@ -86,7 +71,6 @@ void KruskalMST(struct Edge edges[], int V, int E) {
     free(subsets);
 }
 
-// Main function
 int main() {
     int V, E;
     printf("Enter number of vertices and edges: ");
@@ -104,3 +88,4 @@ int main() {
     free(edges);
 return 0;
 }
+
